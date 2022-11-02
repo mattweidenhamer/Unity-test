b09ff1bd78bb2e0a5eea9ee7b2c2a6e6ebea83f0
Matthew Weidenhamer <mattweidenhamer@gmail.com>
2022-11-02 14:22:11 -0400

    After class commit

---
+++ Assets/Scenes/PrintToConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Printing to the console");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //transform.Rotate(0,0,0.01f);
+       //transform.Translate(1,0,0);
+    }
+}
